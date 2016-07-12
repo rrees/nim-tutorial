eb7b80b86f124926fe1e074b3234314408ebdb3c
Robert Rees <shudderfix@gmail.com>
2016-07-12 18:13:03 +0100

    Plays around with the implicit result variable

--- proc.nim
+++ proc.nim
@@ -2,4 +2,11 @@
 proc double(i: int):int =
     return i * i
 
+proc sum_evens(ints: varargs[int]): int =
+    for i in ints:
+        if i mod 2 == 0:
+            result = result + i
+
 echo double(2)
+
+echo sum_evens(1,2,3,4)
